9002705591cd48fd6953b9f0a198c2cdb5d4a28b
Hugo Acosta <thenex@gmail.com>
2023-06-14 15:16:24 -0400

    seed con imagenes para usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+require "open-uri"
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-students = [
-  {
+
+  student_1 = User.new(
     role: false,
     first_name: "Cristóbal",
     last_name: "Selame",
@@ -23,8 +23,12 @@ students = [
     knowledges: "Leyes comunitarias. Derecho penal",
     email: "selame@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_1 = URI.open("https://img.freepik.com/free-photo/portrait-successful-handsome-executive-businessman-smart-casual-wear-looking-camera-smiling_7861-3115.jpg")
+  student_1.photo.attach(io: photo_1, filename: "user_1.png", content_type: "image/png")
+  student_1.save!
+
+  student_2 = User.new(
     role: false,
     first_name: "Juan Manuel",
     last_name: "Binda",
@@ -41,8 +45,12 @@ students = [
     knowledges: "Marketing Digital",
     email: "binda@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_2 = URI.open("https://img.freepik.com/free-photo/young-happy-entrepreneur-writing-whiteboard-while-working-office_637285-10561.jpg")
+  student_2.photo.attach(io: photo_2, filename: "user_2.png", content_type: "image/png")
+  student_2.save!
+
+  student_3 = User.new(
     role: false,
     first_name: "Hugo",
     last_name: "Acosta",
@@ -59,8 +67,12 @@ students = [
     knowledges: "PHP, HTML y CSS",
     email: "acosta@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_3 = URI.open("https://img.freepik.com/free-photo/man-smiling-with-notebook-thumb-up_1187-3448.jpg")
+  student_3.photo.attach(io: photo_3, filename: "user_3.png", content_type: "image/png")
+  student_3.save!
+
+  student_4 = User.new(
     role: false,
     first_name: "Sergio",
     last_name: "Andrade",
@@ -77,8 +89,12 @@ students = [
     knowledges: "Mécanica",
     email: "sergand@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_4 = URI.open("https://img.freepik.com/free-photo/positive-employee-sitting-desk-holding-notebook-high-quality-photo_114579-35666.jpg")
+  student_4.photo.attach(io: photo_4, filename: "user_4.png", content_type: "image/png")
+  student_4.save!
+
+  student_5 = User.new(
     role: false,
     first_name: "Verónica",
     last_name: "Soria",
@@ -95,8 +111,12 @@ students = [
     knowledges: "Platos típicos. Platos Internacionales",
     email: "verosor@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_5 = URI.open("https://img.freepik.com/free-photo/portrait-female-business-executive-holding-file_1170-1118.jpg")
+  student_5.photo.attach(io: photo_5, filename: "user_5.png", content_type: "image/png")
+  student_5.save!
+
+  student_6 = User.new(
     role: false,
     first_name: "Florencia",
     last_name: "Estrada",
@@ -113,8 +133,12 @@ students = [
     knowledges: "Derecho Laboral",
     email: "flor3214@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_6 = URI.open("https://img.freepik.com/free-photo/young-tender-curly-girl-holding-documents_176420-238.jpg")
+  student_6.photo.attach(io: photo_6, filename: "user_6.png", content_type: "image/png")
+  student_6.save!
+
+  student_7 = User.new(
     role: false,
     first_name: "Cecilia",
     last_name: "Rosado",
@@ -131,8 +155,12 @@ students = [
     knowledges: "Posicionamiento de Marca",
     email: "ceci8712@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_7 = URI.open("https://img.freepik.com/free-photo/cheerful-young-employee-celebrating-professional-success_1262-16039.jpg")
+  student_7.photo.attach(io: photo_7, filename: "user_7.png", content_type: "image/png")
+  student_7.save!
+
+  student_8 = User.new(
     role: false,
     first_name: "Andres",
     last_name: "Segundo",
@@ -149,8 +177,12 @@ students = [
     knowledges: "Auditoría Interna",
     email: "andrreseg@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_8 = URI.open("https://img.freepik.com/free-photo/young-handsome-stylish-hipster-man-black-jacket-sitting-office-table-business-style-white-shirt-isolated-working-laptop-start-up-work-place-victory_285396-1486.jpg")
+  student_8.photo.attach(io: photo_8, filename: "user_8.png", content_type: "image/png")
+  student_8.save!
+
+  student_9 = User.new(
     role: false,
     first_name: "Rosario",
     last_name: "Ayala",
@@ -167,8 +199,12 @@ students = [
     knowledges: "Diseño",
     email: "rosa12@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_9 = URI.open("https://img.freepik.com/free-photo/joyful-young-asian-woman-winning-celebrating-victory-achievement-receive-good-news-mobile-phon_1258-123836.jpg")
+  student_9.photo.attach(io: photo_9, filename: "user_9.png", content_type: "image/png")
+  student_9.save!
+
+  student_10 = User.new(
     role: false,
     first_name: "Julio",
     last_name: "Soliz",
@@ -185,8 +221,12 @@ students = [
     knowledges: "Recursos Humanos",
     email: "solizj12@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_10 = URI.open("https://img.freepik.com/free-photo/young-person-during-intership_23-2149315589.jpg")
+  student_10.photo.attach(io: photo_10, filename: "user_10.png", content_type: "image/png")
+  student_10.save!
+
+  student_11 = User.new(
     role: false,
     first_name: "German",
     last_name: "Rocha",
@@ -203,8 +243,12 @@ students = [
     knowledges: "Comercio Internacional",
     email: "germanr981@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_11 = URI.open("https://img.freepik.com/free-photo/smiling-man-pointing_1187-1057.jpg")
+  student_11.photo.attach(io: photo_11, filename: "user_11.png", content_type: "image/png")
+  student_11.save!
+
+  student_12 = User.new(
     role: false,
     first_name: "Raquel",
     last_name: "Rocha",
@@ -221,8 +265,12 @@ students = [
     knowledges: "Administración Financiera",
     email: "raquelroch@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_12 = URI.open("https://img.freepik.com/free-photo/cheerful-pretty-woman-pumping-fist-celebrating-achievement_1262-16140.jpg")
+  student_12.photo.attach(io: photo_12, filename: "user_12.png", content_type: "image/png")
+  student_12.save!
+
+  student_13 = User.new(
     role: false,
     first_name: "Julio",
     last_name: "Roman",
@@ -239,8 +287,12 @@ students = [
     knowledges: "Desarrollo con Python",
     email: "julicor12@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_13 = URI.open("https://img.freepik.com/free-photo/man-working-with-laptop-holding-coffee-cup_1150-52068.jpg")
+  student_13.photo.attach(io: photo_13, filename: "user_13.png", content_type: "image/png")
+  student_13.save!
+
+  student_14 = User.new(
     role: false,
     first_name: "Cristina",
     last_name: "Aponte",
@@ -257,8 +309,12 @@ students = [
     knowledges: "Refinamiento",
     email: "crisap721@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_14 = URI.open("https://img.freepik.com/free-photo/happy-young-business-woman-secretary-holding-laptop-modern-office_1258-104572.jpg")
+  student_14.photo.attach(io: photo_14, filename: "user_14.png", content_type: "image/png")
+  student_14.save!
+
+  student_15 = User.new(
     role: false,
     first_name: "Sofia",
     last_name: "Mercado",
@@ -275,8 +331,12 @@ students = [
     knowledges: "Relaciones Internacionales",
     email: "sofim78@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_15 = URI.open("https://img.freepik.com/free-photo/business-woman-smiling-looking-camera_23-2148218497.jpg")
+  student_15.photo.attach(io: photo_15, filename: "user_15.png", content_type: "image/png")
+  student_15.save!
+
+  student_16 = User.new(
     role: false,
     first_name: "Marcelo",
     last_name: "Rincón",
@@ -293,8 +353,12 @@ students = [
     knowledges: "Fluidos y flujos",
     email: "marcerin@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_16 = URI.open("https://img.freepik.com/free-photo/man-with-beard-showing-thumb-up_1187-5821.jpg")
+  student_16.photo.attach(io: photo_16, filename: "user_16.png", content_type: "image/png")
+  student_16.save!
+
+  student_17 = User.new(
     role: false,
     first_name: "Marcos",
     last_name: "Nuva",
@@ -311,8 +375,12 @@ students = [
     knowledges: "Apoyo Sectorial",
     email: "nuvamarc@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_17 = URI.open("https://img.freepik.com/free-photo/scenery-designers-work_23-2149741846.jpg")
+  student_17.photo.attach(io: photo_17, filename: "user_17.png", content_type: "image/png")
+  student_17.save!
+
+  student_18 = User.new(
     role: false,
     first_name: "Ruben",
     last_name: "Tejerina",
@@ -329,8 +397,12 @@ students = [
     knowledges: "Auditoría Impositiva",
     email: "tejrub87@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_18 = URI.open("https://img.freepik.com/free-photo/smiling-man-with-charts-looking-camera_23-2147707729.jpg")
+  student_18.photo.attach(io: photo_18, filename: "user_18.png", content_type: "image/png")
+  student_18.save!
+
+  student_19 = User.new(
     role: false,
     first_name: "Alba",
     last_name: "Barrientos",
@@ -347,8 +419,12 @@ students = [
     knowledges: "Procesamiento Alimentario",
     email: "barralb3@gmail.com",
     password: "123456"
-  },
-  {
+  )
+  photo_19 = URI.open("https://img.freepik.com/free-photo/brunette-businesswoman-posing_23-2148142722.jpg")
+  student_19.photo.attach(io: photo_19, filename: "user_19.png", content_type: "image/png")
+  student_19.save!
+
+  student_20 = User.new(
     role: false,
     first_name: "Willy",
     last_name: "Suarez",
@@ -365,18 +441,17 @@ students = [
     knowledges: "Recursos Humanos, Desarrollo Humano",
     email: "willys83@gmail.com",
     password: "123456"
-  }
-]
+  )
+  photo_20 = URI.open("https://img.freepik.com/free-photo/young-businessman-happy-expression_1194-1609.jpg")
+  student_20.photo.attach(io: photo_20, filename: "user_20.png", content_type: "image/png")
+  student_20.save!
 
-students.each do |s|
-  User.create!(s)
-end
 
 puts "Students creados"
 
 puts "Creando Companies"
-company = [
-  {
+
+company_21 = User.new(
     role: true,
     phone: 12512345678,
     address: "Calle Principal 142",
@@ -389,8 +464,12 @@ company = [
     about: "TechNode es una empresa de tecnología líder en el desarrollo de soluciones innovadoras.",
     email: "cris@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_21 = URI.open("https://bloggytalky.com/wp-content/uploads/2017/07/create-a-free-logo-design-logo-designs-design-a-free-logo-design-a-free-logo-alltech-just-free-logo-design.png")
+  company_21.photo.attach(io: photo_21, filename: "user_21.png", content_type: "image/png")
+  company_21.save!
+
+  company_22 = User.new(
     role: true,
     phone: 2298765432,
     address: "Avenida General Oviedo 456",
@@ -403,8 +482,12 @@ company = [
     about: "Industreck es una destacada empresa de fabricación que produce bienes de consumo de alta calidad.",
     email: "juanma@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_22 = URI.open("http://lofrev.net/wp-content/photos/2016/07/abstract_logo_vector-1007x919.png")
+  company_22.photo.attach(io: photo_22, filename: "user_22.png", content_type: "image/png")
+  company_22.save!
+
+  company_23 = User.new(
     role: true,
     phone: 35156755482,
     address: "Rua Paulo 789",
@@ -417,8 +500,12 @@ company = [
     about: "Servi Create es una empresa de servicios dedicada a brindar soluciones integrales para sus clientes.",
     email: "hugo@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_23 = URI.open("https://cdn.dribbble.com/users/160383/screenshots/5656135/scott_4x.png")
+  company_23.photo.attach(io: photo_23, filename: "user_23.png", content_type: "image/png")
+  company_23.save!
+
+  company_24 = User.new(
     role: true,
     phone: 441197314722,
     address: "Calle Cuadradi 789",
@@ -431,8 +518,12 @@ company = [
     about: "Consul Range es una empresa de consultoría especializada en estrategias de negocios y desarrollo organizacional.",
     email: "lewagon1@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_24 = URI.open("https://static.vecteezy.com/system/resources/previews/000/606/820/original/c-circle-logo-template-design-vector.jpg")
+  company_24.photo.attach(io: photo_24, filename: "user_24.png", content_type: "image/png")
+  company_24.save!
+
+  company_25 = User.new(
     role: true,
     phone: 5715215894,
     address: "Carrera Costaner 43",
@@ -445,8 +536,12 @@ company = [
     about: "EcoModa es una empresa líder en el sector de la moda, ofreciendo prendas de vestir de alta calidad y diseño vanguardista.",
     email: "lewagon2@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_25 = URI.open("https://static.vecteezy.com/system/resources/previews/000/587/678/non_2x/vector-initial-e-letter-logo-with-swoosh-colored-red-and-black.jpg")
+  company_25.photo.attach(io: photo_25, filename: "user_25.png", content_type: "image/png")
+  company_25.save!
+
+  company_26 = User.new(
     role: true,
     phone: 6036295283,
     address: "Avenida Diametral 14",
@@ -459,8 +554,12 @@ company = [
     about: "Alitron es una empresa dedicada a la producción y distribución de alimentos frescos y saludables para el mercado local e internacional.",
     email: "lewagon3@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_26 = URI.open("https://static.vecteezy.com/system/resources/previews/000/587/678/non_2x/vector-initial-e-letter-logo-with-swoosh-colored-red-and-black.jpg")
+  company_26.photo.attach(io: photo_26, filename: "user_26.png", content_type: "image/png")
+  company_26.save!
+
+  company_27 = User.new(
     role: true,
     phone: 70105428472,
     address: "Calle Soleto 789",
@@ -473,8 +572,12 @@ company = [
     about: "EcoTuris es una agencia de viajes que ofrece experiencias turísticas inolvidables en los destinos más hermosos de Costa Rica.",
     email: "lewagon4@pathfinder.com",
     password: "123456"
-  },
-  {
+  )
+  photo_27 = URI.open("https://static.vecteezy.com/system/resources/previews/002/836/967/large_2x/et-logo-letter-initial-logo-designs-template-with-gold-and-black-background-vector.jpg")
+  company_27.photo.attach(io: photo_27, filename: "user_27.png", content_type: "image/png")
+  company_27.save!
+
+  company_28 = User.new(
     role: true,
     phone: 8911023457,
     address: "Avenida Bartolome 84",
@@ -487,12 +590,10 @@ company = [
     about: "EnergySun es una empresa especializada en el desarrollo y suministro de soluciones energéticas sostenibles para el sector industrial.",
     email: "lewagon5@pathfinder.com",
     password: "123456"
-  }
-]
-
-company.each do |c|
-  User.create(c)
-end
+  )
+  photo_28 = URI.open("https://www.intuswindows.com/wp-content/uploads/2016/04/Logo-energy-conservation-Round_v3.png")
+  company_28.photo.attach(io: photo_28, filename: "user_28.png", content_type: "image/png")
+  company_28.save!
 
 
 puts "Companies creadas"
