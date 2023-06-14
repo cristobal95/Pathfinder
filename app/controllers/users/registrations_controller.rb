95dc7541fbc1192bc31d6980e9c0a8b78b673fc3
Juanma Binda <juanmanuelbinda@gmail.com>
2023-06-14 17:45:15 -0300

    Cloudinary error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,9 +50,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 
   # If you have extra params to permit, append them to the sanitizer.
-  #def configure_account_update_params
+  # def configure_account_update_params
   #  devise_parameter_sanitizer.permit(:account_update, keys: %i[photo pdf])
-  #end
+  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
