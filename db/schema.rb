4a6b5c48643aa45bd649f754a73801d56f3d426e
Juanma Binda <juanmanuelbinda@gmail.com>
2023-06-13 22:18:25 -0300

    Postulation's controller and views finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_220712) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_005159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_220712) do
     t.bigint "internship_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["internship_id"], name: "index_postulations_on_internship_id"
     t.index ["user_id"], name: "index_postulations_on_user_id"
   end
