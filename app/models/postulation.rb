ddbaa83a578014bf74f01109218d64c8ba0e7665
Hugo Acosta <thenex@gmail.com>
2023-06-15 17:14:10 -0400

    arreglo de problemas en show

--- app/models/postulation.rb
+++ app/models/postulation.rb
@@ -2,4 +2,14 @@ class Postulation < ApplicationRecord
   enum status: { Rechazada: 0, Pendiente: 1, Aprobada: 2 }, _default: 1
   belongs_to :user
   belongs_to :internship
+
+  validate :unique_postulation
+
+  private
+
+  def unique_postulation
+    if user.postulations.where(internship_id: internship_id).exists?
+      errors.add(:base, 'Ya te has postulado a esta pasantía.')
+    end
+  end
 end
